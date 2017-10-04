c37cc254214c102ac1c00e4697bd6826cda40eb0
Seth <seth.pacheco@gmail.com>
2017-10-03 19:42:00 -0600

    Adding the content to the first tab

--- MyRuns/app/src/main/java/com/example/i21633/myruns/Tab1StartFragment.kt
+++ MyRuns/app/src/main/java/com/example/i21633/myruns/Tab1StartFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_tab1start.*
 
 /**
  * Created by i21633 on 9/26/17.
@@ -15,7 +16,16 @@ class Tab1StartFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_tab1start, container, false)
+        setClickListeners()
         return rootView
+
+    }
+
+    private fun setClickListeners() {
+        startButton.setOnClickListener({ openManualEntryActivity() })
+    }
+
+    private fun openManualEntryActivity() {
 
     }
 
