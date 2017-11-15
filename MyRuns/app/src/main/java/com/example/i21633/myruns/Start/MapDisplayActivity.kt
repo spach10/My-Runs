b59640524c0c538f5c82559e3ad1171f8bb3b2ba
Seth Pacheco <SPacheco@verisk.com>
2017-11-14 22:56:21 -0700

    Calculate avg speed, current speed, total distance traveled

--- MyRuns/app/src/main/java/com/example/i21633/myruns/Start/MapDisplayActivity.kt
+++ MyRuns/app/src/main/java/com/example/i21633/myruns/Start/MapDisplayActivity.kt
@@ -2,6 +2,7 @@ package com.example.i21633.myruns.Start
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Criteria
 import android.location.Location
@@ -10,6 +11,8 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.example.i21633.myruns.MainActivity
 import com.example.i21633.myruns.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,17 +20,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import org.joda.time.DateTime
+import kotlinx.android.synthetic.main.activity_map_display.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.LocalDateTime
 
 
 class MapDisplayActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mapFragment: SupportMapFragment
-    private lateinit var time : DateTime
+    private lateinit var startTime : LocalDateTime
+    private lateinit var previousTime : LocalDateTime
     private lateinit var startLocation : Location
-    private lateinit var previousLocation : Location
-
+    private  lateinit var previousLocation : Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +41,8 @@ class MapDisplayActivity : AppCompatActivity(), OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
+
+        setClickListeners()
 
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
     }
@@ -50,31 +58,60 @@ class MapDisplayActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
         mMap.isMyLocationEnabled = checkLocationPermission()
-        time = DateTime.now()
-        // Get current location
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.toFloat(), locationListener)
 
+        startTime = LocalDateTime.now()
+        previousTime = startTime
+
+        // Get current location
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val criteria = Criteria()
         val provider = locationManager.getBestProvider(criteria, false)
         val location = locationManager.getLastKnownLocation(provider)
-        val userLocation = LatLng(location.latitude, location.longitude)
-        startLocation = location
-        previousLocation = location
-        //Current location
-        mMap.addMarker(MarkerOptions().position(userLocation).title("My location"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
 
+        startLocation = location
+        previousLocation = startLocation
+
+        val userLocation = LatLng(location.latitude, location.longitude)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1500, 0.toFloat(), locationListener)
+
+        //Current location
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.toFloat()))
+    }
+
+    private fun setClickListeners() {
+        saveGpsEntryButton.setOnClickListener({ v -> saveExerciseEntry(v) })
+        cancelGpsEntryButton.setOnClickListener({ v -> cancelExerciseEntry(v) })
+    }
+
+    private fun saveExerciseEntry(v: View?) {
+
+        var intent = Intent(application, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun cancelExerciseEntry(v: View?) {
+        var intent = Intent(application, MainActivity::class.java)
+        startActivity(intent)
     }
 
     private val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location?) {
-            val timeDif = time .. DateTime.now()
-            val locationDif = location!!.distanceTo(previousLocation)
-            previousLocation = location
-            //var avgSpeed : Int = timeDif / locationDif
+            // Calculate the Avg Speed and display
+            displayAvgSpeed.text = calculateAverageSpeed(location)
+
+            // Calculate Current Speed and display
+            displayCurSpeed.text = calculateCurrentSpeed(location)
+
+            // Calculate the Climb and display
+            displayClimb.text = calculateClimb(location)
+
+            // Calculate Calorie and display
+            displayCaloriesBurned.text = calculateTotalCaloriesBurned()
+
+            // Calculate total distance
+            displayTotalDistance.text = calculateTotalDistance(location)
         }
         override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {}
         override fun onProviderEnabled(p0: String?) {}
@@ -84,6 +121,7 @@ class MapDisplayActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun checkLocationPermission(): Boolean {
         val permission = "android.permission.ACCESS_FINE_LOCATION"
         val res = this.checkCallingOrSelfPermission(permission)
+
         return res == PackageManager.PERMISSION_GRANTED
     }
 
@@ -101,6 +139,46 @@ class MapDisplayActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }// other 'case' lines to check for other
         // permissions this app might request
+    }
+
+    private fun getDifference(start: LocalDateTime, now: LocalDateTime) : Double {
+        val hourDif = Math.abs(start.hour - now.hour)
+        val minDif = Math.abs(start.minute - now.minute)
+        val secDif = Math.abs(start.second - now.second)
+
+        return hourDif.toDouble() + (minDif.toDouble() / 60.toDouble()) + (secDif.toDouble() / 3600.toDouble())
+    }
+
+    private fun calculateAverageSpeed(location : Location?) : String {
+        val timeDifInHours = getDifference(startTime, LocalDateTime.now())
+        val locationDif = location!!.distanceTo(startLocation) / 1000
+        val avgSpeed  = locationDif / timeDifInHours
+
+        return "Avg Speed: " + BigDecimal(avgSpeed).setScale(2, RoundingMode.DOWN).toString() + " km/s"
+    }
+
+    private fun calculateCurrentSpeed (location : Location?) : String {
+        val timeDifInHours = getDifference(previousTime, LocalDateTime.now())
+        previousTime = LocalDateTime.now()  // Set the current time to previous time
+        val locationDif = location!!.distanceTo(startLocation) / 1000
+        val currentSpeed  = locationDif / timeDifInHours
+
+        return "Current Speed: " + BigDecimal(currentSpeed).setScale(2, RoundingMode.DOWN).toString() + " km/s"
+    }
+
+    private fun calculateClimb(location: Location?): String {
+
+
+        return "Climb: 0.0 km"
+    }
+
+    private fun calculateTotalCaloriesBurned(): String {
+        return "Calories: 0"
+    }
+
+    private fun calculateTotalDistance(location : Location?) : String {
+        val distance = Math.abs((location!!.distanceTo(startLocation) / 1000).toDouble())
+        return "Distance: " + BigDecimal(distance).setScale(2, RoundingMode.DOWN) + " km"
     }
 
 }
